7cd7b927c17dd6fa0a3578336c2aaac37f7a969b
Evrixol <56212925+Evrixol@users.noreply.github.com>
2021-03-22 14:12:05 -0600

    Removed commented out or unecessary comments.
    Moved database requirement thingamajig to config.

--- index.js
+++ index.js
@@ -1,25 +1,18 @@
-/*
-here goes our attempt to revive the s0urce.io game
-with a private server and a custom client
+// @NOTE
+// Config.json is liable to not stick around. This is just a placeholder for certain values.
+const config        = require(__dirname+'/config.json');
 
-
-hey nicejs, are you still active?
-I guess not
-
-
-*/
-
-
-const express = require('express');
-const cookieParser = require('cookie-parser');
-const bodyParser = require('body-parser');
-const Database = require("@replit/database");
-var httpServer = require('http').createServer;
+const express       = require('express');
+const cookieParser  = require('cookie-parser');
+const bodyParser    = require('body-parser');
+const Database      = require(config.database);
+var httpServer      = require('http').createServer;
+const { isRegExp }  = require('util');
 var utils = {
-  startPacket: require('./utils/startpacket.js'),
-  playerCreator: require('./utils/playercreator.js'),
-  taskmanager: require('./utils/taskmgr.js'),
-  adRemover: require('./utils/adRemover.js')
+  startPacket       : require('./utils/startpacket.js'),
+  playerCreator     : require('./utils/playercreator.js'),
+  taskmanager       : require('./utils/taskmgr.js'),
+  adRemover         : require('./utils/adRemover.js')
 }
 var players = {
   "1": utils.playerCreator('Server','IN-PROGRESS',5,6969,"Welcome to the S0urce.io Private Server 0.1 Alpha!",69)
@@ -38,113 +31,6 @@ app.get('/', (req, res) => {
   res.sendFile(__dirname+"/client/index.html");
 });
 
-////////////////////////////////////////////////////////////////////////////////////////////// admin panel
-/*
-app.use((req, res, next) => {
-  //get the token from the cookies
-  const authToken = req.cookies['AuthToken'];
-  if (!authToken) return console.error('invalid authentication token X')
-  //inject the user to the request
-  if (authToken) {req.user = authTokens[authToken];}
-  next();
-})
-const users = [
-  // This user is added to the array to avoid creating a new user on each restart
-  {
-    username: 'johndoe@email.com',
-    // This is the SHA256 hash for value of `password`
-    password: 'XohImNooBHFR0OVvjcYpJ3NgPQ1qq73WKhHvch0VQtg='
-  }
-];
-const crypto = require('crypto');
-
-const getHashedPassword = (password) => {
-  const sha256 = crypto.createHash('sha256');
-  const hash = sha256.update(password).digest('base64');
-  return hash;
-}
-const generateAuthToken = () => {
-    return crypto.randomBytes(30).toString('hex');
-}
-const authTokens = {};
-
-app.get('/admin', (req, res) => {
-  res.sendFile(__dirname+"/admin/adminlogin.html");
-})
-
-app.post('/admin/login', (req, res) => {
-  const { email, password } = req.body
-  const hashedPassword = getHashedPassword(password);
-
-  const user = users.find(u => {
-    return u.email === email && hashedPassword === u.password
-  });
-
-  if (user) {
-    const authToken = generateAuthToken();
-    // store authentication token
-    authtokens[authToken] = user;
-    //setting the auth token in cookies
-    res.cookie('AuthToken', authToken);
-    //redirect the user to the admin panel
-    res.redirect('/panel')
-  } else {
-    res.redirect('/admin/login')
-  }
-})
-
-app.get('/protected', (req, res) =>{
-  if (req.user) {
-    res.sendfile(__dirname+"/admin/adminpanel.html");
-  } else {
-    res.redirect('/')
-  }
-});
-
-*/
-//////////////////////////////////////////////////////////////////////////////////////////////// end admin
-
-//                                 why is this commented out?? delete it if you don't need it //////
-//--- just in case somthing breaks
-/*io.on('connection',(socket) => {
-  socket.on('signIn',(c) => {
-    socket.emit('prepareClient',socket.id);
-    utils.startPacket(socket);
-    if(c.name.includes("Cheat")){
-    addPlayer(
-      utils.playerCreator(
-        c.name.replace("Cheat", ""),
-        socket.id,
-        5,
-        150,
-        'dont try to beat me loser',
-        players.length
-      ),
-      socket.id
-    )
-    socketlist.push(socket);
-    }else{
-    addPlayer(
-      utils.playerCreator(
-        c.name,
-        socket.id,
-        0,
-        1,
-        'No quote',
-        Object.keys(players).length,
-        ["not hacked yet",".........."]
-      ),
-      socket.id
-    )
-    socket.playerid=socketlist.push(socket);
-    }
-  })
-  socket.on('disconnect',() => {
-    socketlist.splice(socket.playerid,1)
-    delete players[socket.id];
-    
-  })
-})*/
 io.on('connection',(socket) => {
   var pkgEmit = pkgEmitCreate(socket);
   socket.on('signIn',(data) => {
@@ -251,21 +137,18 @@ function displayPlayers(socket) {
       }
     
     ]
-        
   })
 }
+
 function addPlayer(data,id) {
   players[id] = data;
   
 }
+
 function pkgEmitCreate(socket) {
-return function pkgEmit(...data) {
-socket.emit(
-  {
-    unique: data
+  return function pkgEmit(...data) {
+    socket.emit( { unique: data } )
   }
-)
-}
 }
 
 
@@ -273,6 +156,7 @@ socket.emit(
 app.use('/client',express.static(__dirname+"/client"));
 app.use('/ads', utils.adRemover);
 
-server.listen(3000, () => {
-  console.log('server started');
+server.listen(config.network.port, config.network.hostname, function() 
+{
+  console.log(`Server listening at http://${config.network.hostname}:${config.network.port}/`);
 });
